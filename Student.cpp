420342020e3151272a40d00f79c09727b42aca44
LuuucyXuuu <xu6@cooper.edu>
2017-11-17 21:58:59 -0500

    Completed some of the member functions

--- Student.cpp
+++ Student.cpp
@@ -1,1 +1,34 @@
 #include "Student.h"
+
+Student::Student(std::string fn, std::string ln, int y, Major m){
+	_first_name = fn;
+	_last_name = ln;
+	_grad_year = y;
+	_major = m;
+	return;
+}
+
+Student::~Student(){
+	return;
+}
+
+float Student::getGpa(){
+	float sum = 0;
+	int i;
+	for(i=0,i<_grades.size(),i++){
+	sum = _grades[i] + sum;
+	}
+	return sum / _grades.size();
+}
+
+void Student::addGrade(float grade){
+	_grades.push_back (grade);
+	return;
+	
+}
+std::string Student::majorString(Major m){
+	
+}
+void Student::printInfo(){
+	  
+}
